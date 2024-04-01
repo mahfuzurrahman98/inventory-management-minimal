a0e70ee37597e04b5779541d735bad7ce67137d5
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-01 19:24:16 +0600

    create migration for item

--- server/database/migrations/2024_04_01_131636_create_items_table.php
+++ server/database/migrations/2024_04_01_131636_create_items_table.php
@@ -4,24 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('description');
+            $table->string('image')->nullable(); // Assuming image is a string field containing the image path
+            $table->integer('quantity')->default(0); // Assuming quantity is an integer field
+            $table->foreignId('inventory_id')->constrained();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('items');
     }
 };
