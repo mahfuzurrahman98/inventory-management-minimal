597f5e326cbd610a608463a980602b847babb60d
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-02 00:16:35 +0600

    mapped all auth routes to controller

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+// Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/auth/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+// });
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
