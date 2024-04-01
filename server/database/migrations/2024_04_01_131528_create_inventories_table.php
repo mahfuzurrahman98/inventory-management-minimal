0c7a5bb60c567e378a007392d80e67b49dbc0578
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-01 19:24:08 +0600

    migration created for inventory

--- server/database/migrations/2024_04_01_131528_create_inventories_table.php
+++ server/database/migrations/2024_04_01_131528_create_inventories_table.php
@@ -4,24 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('inventories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('description');
+            $table->foreignId('user_id')->constrained();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('inventories');
     }
 };
