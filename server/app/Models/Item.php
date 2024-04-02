87669db586f61fb34f2029d6e948e212dc88160e
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-03 01:18:24 +0600

    define relationship that item belongs to inventory

--- server/app/Models/Item.php
+++ server/app/Models/Item.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Item extends Model
-{
+class Item extends Model {
     use HasFactory;
+
+    // define relationship with inventory
+    public function inventory() {
+        return $this->belongsTo(Inventory::class);
+    }
 }
