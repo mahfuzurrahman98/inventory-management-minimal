ddbd378d1412d8408e310d4fd081f35ed1b74c30
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-03 01:17:14 +0600

    define ont to many relationship with inventory

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable {
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    // one to many relationship between user and inventory
+    public function inventories() {
+        return $this->hasMany(Inventory::class);
+    }
 }
