05be5b9df88736018bef50ad1b3c7060ee885e48
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-04 03:48:40 +0600

    add pinia and initialize axios interceptor

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { createPinia } from 'pinia';
 import router from './routes';
+import useAuthStore from './stores/auth';
+import { setupAxiosInterceptors } from './api/axios';
 
-createApp(App).use(router).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+const authStore = useAuthStore();
+setupAxiosInterceptors(authStore);
+
+app.mount('#app');
