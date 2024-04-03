c99f087519831b9ca3d068c75034cd586230a2b0
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-04 03:47:03 +0600

    add server URL as proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-
 import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
 
-export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue,html}'],
-    theme: {
-        extend: {},
-    },
+// https://vitejs.dev/config/
+export default defineConfig({
     plugins: [vue()],
-};
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000/api',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+});
