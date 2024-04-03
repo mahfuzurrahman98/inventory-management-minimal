631801b1d7f433a6d5834d07ee499bf39450f3eb
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-04 03:48:06 +0600

    setup pinia store for auth

--- client/src/stores/auth.ts
+++ client/src/stores/auth.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import axios from '../api/axios';
 
 interface IAuth {
     token: string;
@@ -16,6 +17,41 @@ const useUserStore = defineStore('auth', {
         setAuth(auth: IAuth) {
             this.token = auth.token;
             this.name = auth.name;
+        },
+        async logout() {
+            try {
+                await axios.post('/auth/logout', null, {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true,
+                });
+                this.setAuth({
+                    token: '',
+                    name: '',
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        async refresh() {
+            try {
+                const response = await axios.post('/auth/refresh', null, {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true,
+                });
+                console.log('refresh: ', response);
+                const data = response.data.data;
+
+                this.setAuth({
+                    token: data.accessToken,
+                    name: data.user.name,
+                });
+
+                return data.accessToken;
+            } catch (error) {
+                await this.logout();
+                console.log('error from refresh function', error);
+                throw error;
+            }
         },
     },
 });
