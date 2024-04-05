bb0b9a6e5d141582c5ab878938d3fea280deb553
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-06 02:45:33 +0600

    initialize toast

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,18 +5,24 @@ import { createPinia } from 'pinia';
 import router from './routes';
 import useAuthStore from './stores/auth';
 import { setupAxiosInterceptors } from './api/axios';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
-app.use(router);
+app.use(Toast);
 
 const authStore = useAuthStore();
 setupAxiosInterceptors(authStore);
 
-authStore.refresh().catch(error => {
+
+authStore.refresh().catch((error) => {
     console.error(error);
-  });
+});
+
 
 app.mount('#app');
+
+app.use(router);
