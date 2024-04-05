85a4df8b727adbd0146e3c34b39063df680d1ea0
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-06 02:45:54 +0600

    fixed placement for footer contents

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,42 +1,44 @@
 <template>
     <div className="flex flex-col justify-center mt-auto">
-        <div className="border-t-2 mb-2 border-gray-200 py-3 mt-12">
-            <p className="text-center">
-                <a
-                    href="https://mahfuzurrahman98.github.io"
-                    target="blank"
-                    className="text-blue-600 font-semibold"
-                >
-                    Mahfuzur Rahman Arif
-                </a>
-                <span className="ml-2">
-                    | All rights reserved, {{ new Date().getFullYear() }}
-                </span>
-            </p>
-            <div className="flex gap-x-5 justify-center mt-2">
-                <a :href="gmailLink" target="blank">
-                    <img
-                        src="../assets/icons/gmail.svg"
-                        alt=""
-                        className="w-5 md:w-6"
-                    />
-                </a>
-                |
-                <a :href="githubLink" target="blank">
-                    <img
-                        src="../assets/icons/github.svg"
-                        alt=""
-                        className="w-5 md:w-6"
-                    />
-                </a>
-                |
-                <a :href="linkedinLink" target="blank">
-                    <img
-                        src="../assets/icons/linkedin.svg"
-                        alt=""
-                        className="w-5 md:w-6"
-                    />
-                </a>
+        <div className=" border-t-2 mb-2 border-gray-200 py-3 mt-12">
+            <div class="flex flex-col md:flex-row md:justify-between max-w-5xl mx-auto">
+                <p className="text-center">
+                    <a
+                        href="https://mahfuzurrahman98.github.io"
+                        target="blank"
+                        className="text-blue-600 font-medium"
+                    >
+                        Mahfuzur Rahman Arif
+                    </a>
+                    <span className="ml-2">
+                        | All rights reserved, {{ new Date().getFullYear() }}
+                    </span>
+                </p>
+                <div className="flex gap-x-5 justify-center mt-2">
+                    <a :href="gmailLink" target="blank">
+                        <img
+                            src="../assets/icons/gmail.svg"
+                            alt=""
+                            className="w-5 md:w-6"
+                        />
+                    </a>
+                    |
+                    <a :href="githubLink" target="blank">
+                        <img
+                            src="../assets/icons/github.svg"
+                            alt=""
+                            className="w-5 md:w-6"
+                        />
+                    </a>
+                    |
+                    <a :href="linkedinLink" target="blank">
+                        <img
+                            src="../assets/icons/linkedin.svg"
+                            alt=""
+                            className="w-5 md:w-6"
+                        />
+                    </a>
+                </div>
             </div>
         </div>
     </div>
