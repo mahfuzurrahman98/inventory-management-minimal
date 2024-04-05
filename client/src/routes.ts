b5a0f8cd542c12d3ba5de25ef663bdf42ee602d9
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-06 03:19:53 +0600

    routes are defined for all components

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,4 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import useAuthStore from '../src/stores/auth';
+
 import Home from './views/Home.vue';
 import Login from './views/Login.vue';
 import Dashboard from './views/Dashboard.vue';
@@ -10,34 +12,63 @@ const routes: Array<RouteRecordRaw> = [
         path: '/',
         name: 'home',
         component: Home,
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: Login,
-    },
-    {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: Dashboard,
-    },
-    {
-        path: '/inventories',
-        name: 'inventories',
-        component: Inventories,
+        meta: { protected: 0 },
     },
     {
         path: '/register',
         name: 'register',
         component: Register,
+        meta: { protected: -1 },
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: { protected: -1 },
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: { protected: 1 },
+    },
+    {
+        path: '/inventories',
+        name: 'inventories',
+        component: Inventories,
+        meta: { protected: 1 },
     },
 ];
+
+// protected
+// 0, means it is public and does not matter user is logged in or not
+// 1, means it is protected and user should be logged in
+// -1, means it is not protected and user should not be logged in
 
 const router = createRouter({
     history: createWebHistory(''),
     routes,
 });
 
-// router.beforeEach(() => {});
+router.beforeEach(async (to, from, next) => {
+    const _protected = to.meta.protected;
+    const auth = useAuthStore();
+
+    while (auth.isRefreshing) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+
+    const isAuthenticated = auth.token !== '';
+
+    console.log(auth.token);
+
+    if (_protected == 1 && !isAuthenticated) {
+        next({ name: 'login' });
+    } else if (_protected == -1 && isAuthenticated) {
+        next({ name: 'dashboard' });
+    } else {
+        next();
+    }
+});
 
 export default router;
