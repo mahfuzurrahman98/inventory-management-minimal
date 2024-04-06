ff9ffe04426e3fc2454216023627cd02f5036b19
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2024-04-07 04:24:10 +0600

    items route added

--- client/src/views/Items.vue
+++ client/src/views/Items.vue
@@ -1,100 +1,247 @@
 <template>
-    <div class="flex justify-between">
-        <h3 class="text-3xl">Categories</h3>
-        <button
-            class="bg-blue-700 rounded text-white px-3"
-            data-te-toggle="modal"
-            data-te-target="#addModal"
-            data-te-ripple-init
-            data-te-ripple-color="light"
-        >
-            Add New
-        </button>
-    </div>
-    <hr class="mt-3 mb-10" />
-    <div class="flex flex-col w-full">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="overflow-hidden">
-                    <table class="min-w-full text-center">
-                        <thead class="border-b bg-gray-50">
-                            <tr>
-                                <th
-                                    scope="col"
-                                    class="text-sm font-medium text-gray-900 px-6 py-4"
-                                >
-                                    #
-                                </th>
-                                <th
-                                    scope="col"
-                                    class="text-sm font-medium text-gray-900 px-6 py-4"
-                                >
-                                    Name
-                                </th>
-                                <th
-                                    scope="col"
-                                    class="text-sm font-medium text-gray-900 px-6 py-4"
-                                >
-                                    Description
-                                </th>
-                                <th
-                                    scope="col"
-                                    class="text-sm font-medium text-gray-900 px-6 py-4"
-                                >
-                                    Action
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr
-                                v-for="inventory in inventories"
-                                class="bg-white border-b"
-                            >
-                                <td
-                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                                >
-                                    1
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    {{ inventory.name }}
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    <div
-                                        class="form-check flex items-center justify-center"
+    <Layout>
+        <div class="flex justify-between">
+            <h3 class="text-2xl">Items</h3>
+            <router-link
+                to="/items/create"
+                class="bg-blue-700 rounded text-white text-sm px-3 py-1"
+            >
+                Add New
+            </router-link>
+        </div>
+        <hr class="mt-3 mb-10" />
+        <div class="flex flex-col w-full">
+            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div class="overflow-hidden">
+                        <table class="min-w-full">
+                            <thead class="border-b bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
                                     >
-                                        {{ inventory.description }}
-                                    </div>
-                                </td>
-                                <td
-                                    class="text-sm text-center flex justify-center text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                        #
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
+                                    >
+                                        Name
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
+                                    >
+                                        Inventory
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
+                                    >
+                                        Description
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
+                                    >
+                                        Quantity
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        class="text-sm text-left font-medium text-gray-900 px-6 py-4"
+                                    >
+                                        Actions
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="(item, index) in items"
+                                    :key="item.id!"
+                                    class="bg-white border-b"
                                 >
-                                    <div class="flex gap-x-5"></div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                    <td
+                                        class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
+                                    >
+                                        {{
+                                            (currentPage - 1) * limit +
+                                            index +
+                                            1
+                                        }}
+                                    </td>
+                                    <td
+                                        class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                    >
+                                        <div class="flex items-center gap-x-3">
+                                            <img
+                                                v-if="item.image"
+                                                :src="item.image"
+                                                alt="item.name"
+                                                class="w-10 h-10 rounded-sm"
+                                            />
+                                            <p>{{ item.name }}</p>
+                                        </div>
+                                    </td>
+                                    <td
+                                        class="text-sm text-left text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                    >
+                                        {{ item.inventory }}
+                                    </td>
+                                    <td
+                                        class="text-sm text-left text-gray-900 font-light px-6 py-4 whitespace-break-spaces"
+                                    >
+                                        {{
+                                            item.description.substring(0, 35)
+                                        }}...
+                                    </td>
+                                    <td
+                                        class="text-sm text-left text-gray-900 font-light px-6 py-4"
+                                    >
+                                        {{ item.quantity }}
+                                    </td>
+                                    <td
+                                        class="text-sm text-center flex text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                    >
+                                        <div class="flex gap-x-5">
+                                            <router-link
+                                                :to="`/items/${item.id}/edit`"
+                                                class="font-medium text-blue-600 hover:text-blue-900"
+                                            >
+                                                Edit
+                                            </router-link>
+                                            <button
+                                                @click="initDeleteItem(item)"
+                                                class="font-medium text-red-600 hover:text-red-900"
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+
+        <!-- Delete Item Modal -->
+        <Teleport to="body">
+            <modal
+                :show="showDeleteModal"
+                @close="showDeleteModal = false"
+                title="Delete Item"
+            >
+                <template #body>
+                    <div class="flex flex-col gap-y-3">
+                        <p class="text-lg">
+                            Are you sure you want to delete this item?
+                        </p>
+                        <div class="flex justify-end gap-x-3">
+                            <button
+                                @click="
+                                    () => {
+                                        showDeleteModal = false;
+                                        deleteItem(deletedInventroy.id!);
+                                    }
+                                "
+                                class="bg-red-700 text-white px-3 py-1 rounded-md text-md hover:bg-red-600 focus:outline-none focus:shadow-outline-red active:bg-red-800"
+                            >
+                                Yes
+                            </button>
+                            <button
+                                @click="showDeleteModal = false"
+                                class="bg-gray-700 text-white px-3 py-1 rounded-md text-md hover:bg-gray-600 focus:outline-none focus:shadow-outline-gray active:bg-gray-800"
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </template>
+            </modal>
+        </Teleport>
+    </Layout>
 </template>
 
-<script setup>
+<script setup lang="ts">
     import { ref, onMounted } from 'vue';
     import { axiosPrivate } from '../api/axios';
+    import Modal from '../components/Modal.vue';
+    import Layout from '../components/Layout.vue';
+    import formatError from '../helpers/formatError';
+    import { POSITION, useToast } from 'vue-toastification';
+    import { ItemType } from '../types';
 
-    const inventories = ref([]);
+    const items = ref<ItemType[]>([]);
 
-    onMounted(async () => {
-        const response = await axiosPrivate.get('/inventories');
-        const data = await response.data;
-        const msg = await response.data.message;
-        const respData = await response.data.data;
-
-        inventories.value = respData.inventories;
+    const deletedInventroy = ref<ItemType>({
+        id: null,
+        name: '',
+        image: '',
+        description: '',
+        quantity: 0,
+        inventory: '',
+        inventory_id: null,
     });
+
+    const currentPage = ref(1);
+    const limit = ref(2);
+    const totalItems = ref(0);
+
+    const showDeleteModal = ref(false);
+
+    const deleteBtnLoading = ref(false);
+
+    const toast = useToast();
+    const toastOptions: any = {
+        position: POSITION.BOTTOM_RIGHT,
+        timeout: 5000,
+        closeOnClick: false,
+        pauseOnFocusLoss: true,
+        pauseOnHover: true,
+        draggable: false,
+        draggablePercent: 0.6,
+        showCloseButtonOnHover: false,
+        hideProgressBar: false,
+        closeButton: 'button',
+        icon: true,
+        rtl: false,
+    };
+
+    const getItems = async () => {
+        try {
+            const response = await axiosPrivate.get(`/items`);
+            const data = response.data;
+            items.value = data.data.items;
+            totalItems.value = data.data.total;
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const initDeleteItem = (item: ItemType) => {
+        showDeleteModal.value = true;
+        deletedInventroy.value = { ...item };
+    };
+
+    const deleteItem = async (id: Number) => {
+        try {
+            const response = await axiosPrivate.delete(`/items/${id}`);
+            const data = response.data;
+
+            if (data.success) {
+                await getItems();
+                toast.success('Item deleted successfully', toastOptions);
+            }
+        } catch (error) {
+            console.error(error);
+            let errorMessage = formatError(error);
+            toast.error(errorMessage, toastOptions);
+        } finally {
+            deleteBtnLoading.value = false;
+        }
+    };
+
+    onMounted(getItems);
 </script>
